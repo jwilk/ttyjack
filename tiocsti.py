ace2d88a68f8084c1f406c7bab2e297cebee838e
Jakub Wilk <jwilk@jwilk.net>
2016-09-27 12:21:57 +0200

    Refactor.

--- tiocsti.py
+++ tiocsti.py
@@ -1,17 +1,30 @@
+import argparse
 import fcntl
 import os
 import termios
 
-fd = 0
-for fd in range(0, 3):
-    try:
-        fcntl.ioctl(fd, termios.TIOCSTI, ' ')
-    except IOError:
-        continue
+def push(s):
+    fd = 0
+    for fd in range(0, 3):
+        try:
+            fcntl.ioctl(fd, termios.TIOCSTI, s[0])
+        except IOError:
+            continue
+        else:
+            break
     else:
-        break
-else:
-    fd = os.open('/dev/tty', os.O_RDONLY)
-    fcntl.ioctl(fd, termios.TIOCSTI, ' ')
-for ch in 'whoami\n':
-    fcntl.ioctl(fd, termios.TIOCSTI, ch)
+        fd = os.open('/dev/tty', os.O_RDONLY)
+        fcntl.ioctl(fd, termios.TIOCSTI, s[0])
+    for ch in s[1:]:
+        fcntl.ioctl(fd, termios.TIOCSTI, ch)
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('command', metavar='COMMAND')
+    ap.add_argument('rest', nargs='*', help=argparse.SUPPRESS)
+    options = ap.parse_args()
+    cmdline = ' '.join([options.command] + options.rest)
+    push(' {cmd}\n'.format(cmd=cmdline))
+
+if __name__ == '__main__':
+    main()
